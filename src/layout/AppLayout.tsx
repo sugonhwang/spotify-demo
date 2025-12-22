657d5b08d9c8da4af85b61f55d7c9e51c062e11f
sugonhwang <sugonhwang1@gmail.com>
2025-12-22 20:42:00 +0900

    💄 사이드바 메뉴 생성 및 스타일 적용

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,73 @@
-import { Outlet } from "react-router";
+import { Box, styled, Typography } from "@mui/material";
+import { NavLink, Outlet } from "react-router";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
 
+const Layout = styled("div")({
+  display: "flex",
+  height: "100vh",
+  padding: "8px",
+});
+const Sidebar = styled("div")(({ theme }) => ({
+  width: "331px",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
+const ContentBox = styled(Box)(({ theme }) => ({
+  borderRadius: "8px",
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+  width: "100%",
+  padding: "8px",
+  marginBottom: "8px",
+  marginRight: "8px",
+}));
+
+const NavList = styled("ul")({
+  listStyle: "none",
+  padding: 0,
+  margin: 0,
+});
+const StyledNavLink = styled(NavLink)(({ theme }) => ({
+  textDecoration: "none",
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  color: theme.palette.text.secondary,
+  "&:hover": {
+    color: theme.palette.text.primary,
+  },
+  "&.active": {
+    color: theme.palette.text.primary,
+  },
+}));
 const AppLayout = () => {
   return (
-    <div>
-      Sidebar
+    <Layout>
+      <Sidebar>
+        <ContentBox>
+          <NavList>
+            <StyledNavLink to="/">
+              <HomeIcon />
+              <Typography variant="h2" fontWeight={700}>
+                Home
+              </Typography>
+            </StyledNavLink>
+            <StyledNavLink to="/search">
+              <SearchIcon />
+              <Typography variant="h2" fontWeight={700}>
+                Search
+              </Typography>
+            </StyledNavLink>
+          </NavList>
+        </ContentBox>
+      </Sidebar>
       <Outlet />
-    </div>
+    </Layout>
   );
 };
 
