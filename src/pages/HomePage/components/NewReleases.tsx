fb7ad683616d5a695c67ecbf50ff64b2d31c3143
sugonhwang <sugonhwang1@gmail.com>
2025-12-25 16:12:25 +0900

    💄 Add to New Released Albums Componet style

---
+++ src/pages/HomePage/components/NewReleases.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+const NewReleases = () => {
+  return (
+    <div>
+      <Typography variant="h1" paddingTop="8px">
+        New Released Albums
+      </Typography>
+    </div>
+  );
+};
+
+export default NewReleases;
