07b61d785c2f90e701822faecf6cf2dba61bab37
sugonhwang <sugonhwang1@gmail.com>
2025-12-27 03:08:18 +0900

    ✨ feat: token 응답 에러 처리 및 API 호출 기능 구현

--- src/hooks/useGetNewReleases.ts
+++ src/hooks/useGetNewReleases.ts
@@ -1,11 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getNewReleases } from "../apis/albumApi";
+import useClientCredentialToken from "./useClientCredentialToken";
 
 const useGetNewReleases = () => {
+  // token이 있어야 API 호출이 가능하므로 token을 가져와야 함
+  const clientCredentialToken = useClientCredentialToken();
+
   return useQuery({
     queryKey: ["new-releases"],
     queryFn: async () => {
-      return getNewReleases();
+      if (!clientCredentialToken) {
+        throw new Error("No token available"); // clientCredentialToken이 undefined의 대한 use case 처리 필요
+      }
+
+      return getNewReleases(clientCredentialToken);
     },
+    enabled: !!clientCredentialToken, // 토큰이 있을 때만 API 호출
   });
 };
+
+export default useGetNewReleases;
