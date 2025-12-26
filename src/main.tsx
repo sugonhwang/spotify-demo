9508f3058974f352c261b312f8e93b304a8dff8c
sugonhwang <sugonhwang1@gmail.com>
2025-12-26 21:06:26 +0900

    ♻️ refact: QueryClientProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,18 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import theme from "./theme.ts";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
