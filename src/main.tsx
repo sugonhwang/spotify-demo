778dbefa153dcec2401a22b01f3a0f06db66a1d8
sugonhwang <sugonhwang1@gmail.com>
2025-12-27 03:10:10 +0900

    ♻️ refactor: QueryClientProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import theme from "./theme.ts";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(); // 리엑트 쿼리 옵션 세팅
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
