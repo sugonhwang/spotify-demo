74b9ca11822f09496d15487827bd00baa98e426d
sugonhwang <sugonhwang1@gmail.com>
2025-12-27 03:02:22 +0900

    ✨ feat: Add  Api Response

--- src/apis/albumApi.ts
+++ src/apis/albumApi.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
+import { GetNewReleasesResponse } from "../models/album";
 
-export const getNewReleases = async () => {
+export const getNewReleases = async (clientCredentialToken: string): Promise<GetNewReleasesResponse> => {
+  console.log("url", SPOTIFY_BASE_URL);
   try {
-    const response = await axios.get(``);
-  } catch (error) {}
+    const response = await axios.get(`${SPOTIFY_BASE_URL}/browse/new-releases?limit=6`, {
+      headers: {
+        Authorization: `Bearer ${clientCredentialToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("fail to fetch new releases data", error);
+    throw new Error("fail to fetch new releases");
+  }
 };
--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -3,22 +3,28 @@ import { clientId, clientSecret } from "../configs/authConfig";
 import { ClientCredentialTokenResponse } from "../models/auth";
 
 const encodedBase64 = (data: string): string => {
-  return Buffer.from(data).toString("base64");
+  // 환경에 따른 인코딩 방법 구분
+  if (typeof window !== "undefined") {
+    return btoa(data); // btoa: 각 문자가 1바이트(0~255)로 해석 가능한 문자열을 Base64로 인코딩한 ASCII 문자열 생성
+  } else {
+    return Buffer.from(data).toString("base64");
+  }
 };
 
 export const getClientCredentialToken = async (): Promise<ClientCredentialTokenResponse> => {
   try {
     const body = new URLSearchParams({
-      grant_typ: "client_credentials",
+      grant_type: "client_credentials",
     });
     const response = await axios.post("https://accounts.spotify.com/api/token", body, {
       headers: {
-        Authorization: `Basic ${encodedBase64(clientId + ":" + clientSecret)}`,
+        Authorization: `Basic ${encodedBase64(`${clientId}:${clientSecret}`)}`,
         "Content-Type": "application/x-www-form-urlencoded",
       },
     });
     return response.data;
   } catch (error) {
+    console.error("Failed to fetch client credential token:", error);
     throw new Error("Fail to fetch client Credential Token");
   }
 };
