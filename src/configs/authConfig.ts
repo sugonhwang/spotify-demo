298f48a3a31c337cd278083520cf16663ba75876
sugonhwang <sugonhwang1@gmail.com>
2025-12-30 03:24:07 +0900

    🔧 configure: SCOPES 추가

--- src/configs/authConfig.ts
+++ src/configs/authConfig.ts
@@ -1,3 +1,4 @@
 export const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 export const CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_SECRET_ID;
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+export const SCOPES = "playlist-read-private user-read-private user-read-email"; // API 호출할때 마다 필요한 권한 요청시 필요함
